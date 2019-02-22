d1e4565100d1cfed118d6dd0245491ee0b318aca
Bhavir Shah <shahbhavir@gmail.com>
2019-02-22 14:43:01 +0530

    feat: add example using next.js

---
+++ examples/using-next/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    // Will be available on both server and client
+    MATIC_PROVIDER: 'https://testnet.matic.network',
+    PARENT_PROVIDER: 'https://kovan.infura.io/matic',
+    ROOTCHAIN_ADDRESS: '0x24e01716a6ac34d5f2c4c082f553d86a557543a7',
+    MATIC_WETH_ADDRESS: '0xeD3CAFb4dCf835E7a1D2E3169F8D296f931b4aA7',
+    SYNCER_URL: 'https://eth-syncer.api.matic.network/api/v1',
+    WATCHER_URL: 'https://eth-watcher.api.matic.network/api/v1',
+  },
+}
---
+++ examples/using-next/pages/index.js
@@ -0,0 +1,79 @@
+// Import Matic sdk
+import { useState } from 'react'
+import Matic from 'maticjs'
+
+let matic = null
+
+// Create sdk instance
+function initMatic() {
+  if (!matic) {
+    matic = new Matic({
+      maticProvider: process.env.MATIC_PROVIDER,
+      parentProvider: process.env.PARENT_PROVIDER,
+      rootChainAddress: process.env.ROOTCHAIN_ADDRESS,
+      maticWethAddress: process.env.MATIC_WETH_ADDRESS,
+      syncerUrl: process.env.SYNCER_URL,
+      watcherUrl: process.env.WATCHER_URL,
+    })
+
+    matic.wallet = '0x19D8234908CD1089C096361FFAB11EF93869EDE52795B5C310590D8A7C3A4A01' // your private key
+  }
+}
+
+const from = '0x5784d63560319839e5696a283096e169d1F4E659' // from address
+const token = '0xC4375B7De8af5a38a93548eb8453a498222C4fF2' // test token address
+const recepient = '0x7ed7f36694153ba6eff6ca6726b60f6e2bb17fcf' // test recepient address
+const amount = '10000000000000000'
+
+function Home() {
+  const [hash, setHash] = useState(null)
+
+  const transferTokens = () => {
+    // Send Tokens
+    matic.transferTokens(token, recepient, amount, {
+      from,
+      onTransactionHash: (resp) => {
+        // action on Transaction success
+        // eslint-disable-next-line
+        console.log('Transaction hash:', resp)
+        setHash(resp)
+      },
+    })
+  }
+
+  initMatic()
+
+  return (
+    <div className="parent">
+      <div>Welcome to Matic.js!</div>
+      <button onClick={transferTokens} type="button">
+        Transfer
+      </button>
+      {hash ?
+        (
+          <div>
+            Your latest transaction:{' '}
+            <a
+              href={`https://testnet1-explorer.matic.network/#/transaction/${hash}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {hash}
+            </a>
+          </div>
+        ) : null
+      }
+      <style jsx>{`
+      .parent {
+        font: 15px Helvetica, Arial, sans-serif;
+        background: papayawhip;
+        padding: 100px;
+        text-align: center;
+        transition: 100ms ease-in background;
+      }
+    `}</style>
+    </div>
+  )
+}
+
+export default Home
