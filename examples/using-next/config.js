809748e4d356167e06e3d21149a9bbcc3b11a455
lebed2045 <lebed2045@protonmail.com>
2019-05-19 12:15:43 -0400

    removed env, small refactoring

---
+++ examples/using-next/config.js
@@ -0,0 +1,18 @@
+const config = {
+  MATIC_PROVIDER: 'https://testnet2.matic.network',
+  PARENT_PROVIDER:
+    'https://ropsten.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc',
+  ROOTCHAIN_ADDRESS: '0x60e2b19b9a87a3f37827f2c8c8306be718a5f9b4',
+  WITHDRAWMANAGER_ADDRESS: '0x4ef2b60cdd4611fa0bc815792acc14de4c158d22',
+  DEPOSITMANAGER_ADDRESS: '0x4072fab2a132bf98207cbfcd2c341adb904a67e9',
+  SYNCER_URL: 'https://matic-syncer2.api.matic.network/api/v1',
+  WATCHER_URL: 'https://ropsten-watcher2.api.matic.network/api/v1',
+  ROOTWETH_ADDRESS: '0x421dc9053cb4b51a7ec07b60c2bbb3ec3cfe050b',
+  MATIC_WETH_ADDRESS: '0x31074c34a757a4b9FC45169C58068F43B717b2D0',
+  ROPSTEN_TEST_TOKEN: '0x6b0b0e265321e788af11b6f1235012ae7b5a6808',
+  MATIC_TEST_TOKEN: '0xcc5de81d1af53dcb5d707b6b33a50f4ee46d983e',
+  ROPSTEN_ERC721_TOKEN: '0x07d799252cf13c01f602779b4dce24f4e5b08bbd',
+  MATIC_ERC721_TOKEN: '0x9f289a264b6db56d69ad53f363d06326b984e637'
+};
+
+export default config;
--- examples/using-next/utils/store.js
+++ examples/using-next/utils/store.js
@@ -1,34 +1,50 @@
 import Matic from 'maticjs'
+import config from '../config'
+import Web3 from 'web3';
 
-const from = '0x87b917F40f7a031e13577200801b5f2f0D3E1b91' // from address
-const token = '0x6b0b0e265321e788af11b6f1235012ae7b5a6808' // test token address
+const from = '0x87b917F40f7a031e13577200801b5f2f0D3E1b91'; // from address
+const token = '0x6b0b0e265321e788af11b6f1235012ae7b5a6808'; // test token address
 
-let matic = null
+class MaticSingleton {
+  static instance = null;
 
-// Create sdk instance
-function initMatic() {
-  if (!matic) {
-    matic = new Matic({
-      maticProvider: process.env.MATIC_PROVIDER,
-      parentProvider: process.env.PARENT_PROVIDER,
-      rootChainAddress: process.env.ROOTCHAIN_ADDRESS,
-      maticWethAddress: process.env.MATIC_WETH_ADDRESS,
-      syncerUrl: process.env.SYNCER_URL,
-      watcherUrl: process.env.WATCHER_URL,
-      withdrawManagerAddress: process.env.WITHDRAWMANAGER_ADDRESS,	
-    })
+  constructor(privateKey) {
+    if (!privateKey) {
+      throw new Error(`Please specify private key for the transaction signature. Got ${privateKey}`)
+    }
+    const matic = new Matic({
+      maticProvider: config.MATIC_PROVIDER,
+      parentProvider: config.PARENT_PROVIDER,
+      rootChainAddress: config.ROOTCHAIN_ADDRESS,
+      maticWethAddress: config.MATIC_WETH_ADDRESS,
+      syncerUrl: config.SYNCER_URL,
+      watcherUrl: config.WATCHER_URL,
+      withdrawManagerAddress: config.WITHDRAWMANAGER_ADDRESS,
+    });
+    matic.wallet = privateKey;
+    //TODO: figure out whether the address is necessary
+    const web3 = new Web3(config.PARENT_PROVIDER);
+    const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+    const fromAddress = account.address;
+  }
 
-    matic.wallet = '<private-key>' // your private key
+  static getInstance(privateKey) {
+    if (!MaticSingleton.instance) {
+      MaticSingleton.instance = new MaticSingleton(privateKey);
+    }
+    return MaticSingleton.instance;
   }
 }
 
+
 const Store = () => {
-  initMatic()
+  const privateKey = process.env.PRIVATE_KEY || "0x807BABA1E3243457A5AACF323AAC201709A46692D28C8806EFB111E31639A111";
+  const matic = MaticSingleton.getInstance(privateKey);
   return {
     matic,
     from,
     token,
   }
-}
+};
 
 export default new Store()
