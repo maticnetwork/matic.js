6b570307f1fb4e528c2bcf46ed7181a092f6440c
Bhavir Shah <shahbhavir@gmail.com>
2019-06-21 20:37:25 +0530

    fix: change MATIC_WETH_ADDRESS in using-nuxt example

--- examples/using-nuxt/nuxt.config.js
+++ examples/using-nuxt/nuxt.config.js
@@ -24,7 +24,7 @@ export default {
       'https://ropsten.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc',
     ROOTCHAIN_ADDRESS: '0x60e2b19b9a87a3f37827f2c8c8306be718a5f9b4',
     WITHDRAWMANAGER_ADDRESS: '0x4ef2b60cdd4611fa0bc815792acc14de4c158d22',
-    MATIC_WETH_ADDRESS: '0x74f2a31a044c87bd687f2dcd5f858940f9c28d0c',
+    MATIC_WETH_ADDRESS: '0x31074c34a757a4b9FC45169C58068F43B717b2D0',
     SYNCER_URL: 'https://matic-syncer2.api.matic.network/api/v1',
     WATCHER_URL: 'https://ropsten-watcher2.api.matic.network/api/v1',
   },
