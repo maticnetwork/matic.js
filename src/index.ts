5b414700418120c95b89bdc18096de0c5c859800
atvanguard <93arpit@gmail.com>
2019-09-12 11:14:53 +0530

    estimateGas before sending a tx

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 import Web3 from 'web3'
 import DepositManager from './root/DepositManager'
+import { address, SendOptions } from './types/Common'
 
 export default class Matic {
   _web3: any
   _parentWeb3: any
   public depositManager: DepositManager
 
-  constructor(options: any = {}) {
-    // this._throwIfNull(options.maticProvider, 'maticProvider is required')
-    // this._throwIfNull(options.parentProvider, 'parentProvider is required')
-
+  constructor(options: any = {}, _defaultOptions?: SendOptions) {
     this._web3 = new Web3(options.maticProvider)
     this._web3.matic = true
-    this._parentWeb3 = new Web3(options.parentProvider)
+    this._parentWeb3 = options.parentWeb3 || new Web3(options.parentProvider)
 
-    this.depositManager = new DepositManager(options.depositManagerAddress, this._parentWeb3)
+    this.depositManager = new DepositManager(options.depositManagerAddress, this._parentWeb3, _defaultOptions)
   }
 }
--- src/root/DepositManager.ts
+++ src/root/DepositManager.ts
@@ -12,8 +12,23 @@ export default class DepositManager {
     this._defaultOptions = _defaultOptions || {}
   }
 
-  depositERC20(token: address, amount: BigNumber, options?: SendOptions) {
-    return this._depositManagerContract.methods.depositERC20(token, amount)
-      .send(options || this._defaultOptions)
+  depositERC20(token: address, amount: string, options?: SendOptions) {
+    return this._send(
+      this._depositManagerContract.methods.depositERC20(token, amount),
+      options
+    )
+  }
+
+  setDepositManagerAddress(_depositManagerContractAddress: address) {
+    this._depositManagerContract.options.address = _depositManagerContractAddress
+  }
+
+  private async _send(method, options) {
+    const _options = options || this._defaultOptions
+    // since we use the delegated proxy patterns, the following should be a good way to provide enough gas
+    // apparently even when provided with a buffer of 20k, the call reverts. This shouldn't be happening because the actual gas used is less than what the estimation returns
+    // providing higher buffer for now
+    _options.gas = (await method.estimateGas()) + 200000
+    return method.send(_options)
   }
 }
