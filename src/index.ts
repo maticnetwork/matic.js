f343a0569f3fdcc179b77c8be4dda3c6d040a260
atvanguard <93arpit@gmail.com>
2019-09-20 19:07:02 +0530

    update depositManager

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import Web3 from 'web3'
 import DepositManager from './root/DepositManager'
-import { address, SendOptions } from './types/Common'
+import { SendOptions } from './types/Common'
 
 export default class Matic {
   _web3: any
-  _parentWeb3: any
+  public parentWeb3: any
   public depositManager: DepositManager
 
   constructor(options: any = {}, _defaultOptions?: SendOptions) {
     this._web3 = new Web3(options.maticProvider)
     this._web3.matic = true
-    this._parentWeb3 = new Web3(options.parentProvider)
+    this.parentWeb3 = new Web3(options.parentProvider)
 
-    this.depositManager = new DepositManager(options.depositManagerAddress, this._parentWeb3, _defaultOptions)
+    if (options.depositManagerAddress) {
+      this.depositManager = new DepositManager(options.depositManagerAddress, this.parentWeb3, _defaultOptions)
+    }
   }
 }
