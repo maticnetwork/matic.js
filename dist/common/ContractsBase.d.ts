53d46c79595d31388649f1dd9dc66f66eb6acf14
ptsayli@gmail.com <ptsayli@gmail.com>
2019-12-07 12:02:58 +0530

    remove dist folder and add to gitignore

--- dist/common/ContractsBase.d.ts
+++
@@ -1,7 +0,0 @@
-import Web3Client from './Web3Client';
-import BN from 'bn.js';
-export default class ContractsBase {
-    web3Client: Web3Client;
-    constructor(web3Client: Web3Client);
-    encode(number: BN | string): string;
-}
