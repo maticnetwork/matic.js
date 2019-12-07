ee81535b9c9e14c2503b96d39cc587678c7da312
ptsayli@gmail.com <ptsayli@gmail.com>
2019-12-07 12:41:00 +0530

    update examples

--- examples/node/approve-ERC20.js
+++ examples/node/approve-ERC20.js
@@ -1,6 +1,6 @@
 const bn = require('bn.js')
 
-const Matic = require('../../dist/index').default
+const Matic = require('@maticnetwork/maticjs').default
 const config = require('./config')
 const token = config.ROPSTEN_TEST_TOKEN // test token address
 // const amount = '1000000000000000000' // amount in wei
