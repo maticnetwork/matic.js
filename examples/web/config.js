b041fa77628e66a3035a3584ea884a6321efbcd4
ptsayli@gmail.com <ptsayli@gmail.com>
2019-12-26 16:02:18 +0530

    remove web, nuxt, next examples

--- examples/web/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  'MATIC_PROVIDER': 'https://testnet2.matic.network/',
-  'PARENT_PROVIDER': 'https://ropsten.infura.io/v3/70645f042c3a409599c60f96f6dd9fbc',
-  'ROOTCHAIN_ADDRESS': '0x60e2b19b9a87a3f37827f2c8c8306be718a5f9b4',
-  'WITHDRAWMANAGER_ADDRESS': '0x4ef2b60cdd4611fa0bc815792acc14de4c158d22',
-  'MATIC_WETH_ADDRESS': '0x74f2a31a044c87bd687f2dcd5f858940f9c28d0c',
-  'SYNCER_URL': 'https://matic-syncer2.api.matic.network/api/v1',
-  'WATCHER_URL': 'https://ropsten-watcher2.api.matic.network/api/v1',
-}
