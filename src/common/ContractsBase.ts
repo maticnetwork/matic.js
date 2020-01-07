c3b69146ce9338a61811da5232d1d6694a3526b0
atvanguard <93arpit@gmail.com>
2020-01-07 13:02:29 +0530

    Fix encode

--- src/common/ContractsBase.ts
+++ src/common/ContractsBase.ts
@@ -13,12 +13,15 @@ export default class ContractsBase {
     this.web3Client = web3Client
   }
 
-  public encode(number: BN | string) {
+  public encode(number: BN | string | number) {
+    if (typeof number === 'number') {
+      number = new BN(number)
+    } else if (typeof number === 'string') {
+      if (number.slice(0, 2) === '0x') return number
+      number = new BN(number)
+    }
     if (BN.isBN(number)) {
       return '0x' + number.toString(16)
-    } else if (typeof number === 'string') {
-      assert.equal(number.slice(0, 2), '0x', 'expected a 0x prefixed string')
-      return number
     }
   }
 
