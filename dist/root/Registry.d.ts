bf1627f88974e68d3cb9a31c8e6679de3a9ddecc
ptsayli@gmail.com <ptsayli@gmail.com>
2020-02-24 13:30:30 +0530

    Remove build files

--- dist/root/Registry.d.ts
+++
@@ -1,8 +0,0 @@
-import Contract from 'web3/eth/contract';
-import ContractsBase from '../common/ContractsBase';
-import { address } from '../types/Common';
-import Web3Client from '../common/Web3Client';
-export default class Registry extends ContractsBase {
-    registry: Contract;
-    constructor(registry: address, web3Client: Web3Client);
-}
--- dist/types/Common.d.ts
+++
@@ -1,9 +0,0 @@
-export declare type address = string;
-export declare interface SendOptions {
-    from?: string;
-    value?: string | number;
-    gas?: string | number;
-    gasPrice?: string | number;
-    encodeAbi?: boolean;
-    parent?: boolean;
-}
