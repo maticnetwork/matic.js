e7df42adc74b376b161716901117c0cb64f41ad5
Arth Patel <arth@matic.network>
2020-06-25 19:27:37 +0530

    chg: add pos token getters in contract base

--- src/common/ContractsBase.ts
+++ src/common/ContractsBase.ts
@@ -4,6 +4,10 @@ import Web3Client from './Web3Client'
 
 import { address } from '../types/Common'
 
+import posChildERC20Artifact from 'matic-pos-portal/artifacts/ChildERC20.json'
+import posChildERC721Artifact from 'matic-pos-portal/artifacts/ChildERC20.json'
+import posChildERC1155Artifact from 'matic-pos-portal/artifacts/ChildERC20.json'
+
 export default class ContractsBase {
   static MATIC_CHILD_TOKEN: address = '0x0000000000000000000000000000000000001010'
 
@@ -39,5 +43,20 @@ export default class ContractsBase {
 
   public getChildMaticContract() {
     return new this.web3Client.web3.eth.Contract(this.network.abi('MRC20'), ContractsBase.MATIC_CHILD_TOKEN)
+  }
+
+  public getPOSERC20TokenContract(token: address, parent: boolean = false) {
+    const web3 = parent ? this.web3Client.parentWeb3 : this.web3Client.web3
+    return new web3.eth.Contract(posChildERC20Artifact.abi, token)
+  }
+
+  public getPOSERC721TokenContract(token: address, parent: boolean = false) {
+    const web3 = parent ? this.web3Client.parentWeb3 : this.web3Client.web3
+    return new web3.eth.Contract(posChildERC721Artifact.abi, token)
+  }
+
+  public getPOSERC1155TokenContract(token: address, parent: boolean = false) {
+    const web3 = parent ? this.web3Client.parentWeb3 : this.web3Client.web3
+    return new web3.eth.Contract(posChildERC1155Artifact.abi, token)
   }
 }
