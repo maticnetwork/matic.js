6d247e01d352464dfbd79bcf66614e8c1c6c74a0
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-07-25 13:31:22 +0530

    add pos config

--- examples/config.js
+++ examples/config.js
@@ -20,6 +20,19 @@ module.exports = {
   child: {
     rpc: 'https://rpc-mumbai.matic.today', // This is the MATIC testnet RPC
   },
+  pos: {
+    parent: {
+      erc20: '0x655f2166b0709cd575202630952d71e2bb0d61af',
+      erc721: '0x5a08d01e07714146747950CE07BB0f741445D1b8',
+      erc1155: '0x2e3Ef7931F2d0e4a7da3dea950FF3F19269d9063',
+    },
+    child: {
+      erc721: '0xEC8CB8bBb069470bC358ffB0e3710c64830da383',
+      erc20: '0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e',
+      weth: '0x714550C2C1Ea08688607D86ed8EeF4f5E4F22323',
+      erc1155: '0xA07e45A987F19E25176c877d98388878622623FA',
+    },
+  },
   SYNCER_URL: 'https://testnetv3-syncer.api.matic.network/api/v1', // Backend service which syncs the Matic sidechain state to a MySQL database which we use for faster querying. This comes in handy especially for constructing withdrawal proofs while exiting assets from Plasma.
   WATCHER_URL: 'https://testnetv3-watcher.api.matic.network/api/v1', // Backend service which syncs the Matic Plasma contract events on Ethereum mainchain to a MySQL database which we use for faster querying. This comes in handy especially for listening to asset deposits on the Plasma contract.
   ROOTWETH_ADDRESS: '0x7BdDd37621186f1382FD59e1cCAE0316F979a866', // This is a wrapped ETH ERC20 contract address so that we can support ETH deposits to the sidechain
