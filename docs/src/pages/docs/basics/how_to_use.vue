280a84decccbc7880f90519e212be3cc99dd520e
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-10-09 14:03:11 +0530

    rm pages

--- docs/src/pages/docs/basics/how_to_use.vue
+++
@@ -1,7 +0,0 @@
-<template><Layout title='undefined' description='undefined' keywords='undefined' contentSrc='/home/warrior/projects/opensource/matic.js/docs/content/docs/basics/how_to_use.md'></Layout></template>
-        <script>import Layout from '/home/warrior/projects/opensource/matic.js/docs/layouts/docs.vue'
-        export default {
-            components:{Layout}
-        };
-        </script>
-        
