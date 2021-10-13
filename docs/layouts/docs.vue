a34c72f2da0981f96d252eca5cd160648afaf6d5
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-10-13 09:36:30 +0530

    rm ads var

--- docs/layouts/docs.vue
+++ docs/layouts/docs.vue
@@ -71,7 +71,6 @@ export default {
     Links,
   },
   created() {
-    this.ads = ['Component based framework for nodejs']
     this.finder = new FlexSearch({
       encode: 'balance',
       tokenize: 'forward',
