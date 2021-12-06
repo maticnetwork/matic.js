20b7ee81e6804555dc9cdd145b54e6b256ae2662
ujjwalgupta94 <ujjwal@matic.network>
2021-12-06 16:07:00 +0530

    enable footer

--- docs/content/footer.vue
+++ docs/content/footer.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="b-footer row">
-    <div class="col-lg-8">
-      <h4>Help Us Grow</h4>
+    <div class="col-sm-12 col-lg-8">
       <div class="row">
-        <button class="btn">Sponsor Us</button>
-        <button class="btn ml-10px">Be a backer</button>
+        <div class="col-sm-12 col-lg-4">
+          <h5>Versions</h5>
+          <div>
+            <a href="/v2/docs/introduction">version 2</a>
+          </div>
+        </div>
+        <div class="col-sm-12 col-lg-4">
+          <h5>Ecosystem</h5>
+          <div><a href="https://github.com/maticnetwork/maticjs-web3">maticjs-web3</a></div>
+          <div>
+            <a href="https://github.com/maticnetwork/maticjs-ethers">maticjs-ethers</a>
+          </div>
+          <div>
+            <a href="https://github.com/maticnetwork/maticjs-plasma">maticjs-plasma</a>
+          </div>
+          <div>
+            <a href="https://github.com/fx-portal/maticjs-fxportal">maticjs-fxportal</a>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="col-lg-4">
+    <div class="col-sm-12 col-lg-4">
       Created with
-      <i class="fas fa-heart" style="color: red"></i> by
-      <a target="_blank" href="https://ujjwalguptaofficial.github.io/"
-        >Ujjwal Gupta</a
-      >
+      <i class="fas fa-heart" style="color: red;"></i> by
+      <a style="color: inherit;" target="_blank" href="https://polygon.technology/">Polygon technology</a>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
-<style>
+<style lang="scss">
 .b-footer {
   margin-top: 20px;
   background: var(--primary-color);
   color: white;
   padding: 20px;
+  a {
+    color: #d3c4ed;
+    text-decoration: underline;
+  }
 }
 </style>
