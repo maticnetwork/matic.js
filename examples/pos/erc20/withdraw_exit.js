e96bb2b42d1a9ae94854627ea4926760177094ca
nitinmittal23 <nitinmittal44066@gmail.com>
2022-02-17 12:15:15 +0530

    new: example for different burnEventSig

--- examples/pos/erc20/withdraw_exit.js
+++ examples/pos/erc20/withdraw_exit.js
@@ -7,6 +7,13 @@ const execute = async () => {
 
   const result = await erc20Token.withdrawExit('0xb005d8db45f33836c422ee18286fa8ebe49b4ec7b9930e673d85ecd081cc3b8e');
 
+  /*  If burn event Signature is different for the particular transaction. */
+  // const burnEventSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+  // const result = await erc20Token.withdrawExit('0x911403585328c9e1d8fa3354ed78b51772684bf755dda19d2658247924af473d', {
+  //   burnEventSignature: burnEventSignature,
+  // });
+  // console.log(result)
+
   const txHash = await result.getTransactionHash();
   console.log("txHash", txHash);
   const receipt = await result.getReceipt();
