2f25af1dd6f921f950b45ec3ce62ece0c7e2bb59
nitinmittal23 <nitinmittal44066@gmail.com>
2022-02-17 12:27:23 +0530

    new: file for custom event exit

--- examples/pos/erc20/withdraw_exit_custom_event.js
+++ examples/pos/erc20/withdraw_exit_custom_event.js
@@ -5,7 +5,11 @@ const execute = async () => {
   const client = await getPOSClient();
   const erc20Token = client.erc20(pos.parent.erc20, true);
 
-  const result = await erc20Token.withdrawExit('0xb005d8db45f33836c422ee18286fa8ebe49b4ec7b9930e673d85ecd081cc3b8e');
+  const burnEventSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+
+  const result = await erc20Token.withdrawExit('0x911403585328c9e1d8fa3354ed78b51772684bf755dda19d2658247924af473d', {
+    burnEventSignature: burnEventSignature,
+  });
 
   const txHash = await result.getTransactionHash();
   console.log("txHash", txHash);
