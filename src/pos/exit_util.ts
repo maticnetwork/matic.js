48e374e45f041040ba24c9ec280e32206edacb43
Rajesh Chaganti <40339800+py-zoid@users.noreply.github.com>
2022-06-15 15:27:25 +0530

    Add case for erc721BatchTransfer in getLogIndices

--- src/pos/exit_util.ts
+++ src/pos/exit_util.ts
@@ -84,6 +84,15 @@ export class ExitUtil {
                     logIndices.push(index), logIndices), []
             );
             break;
+          
+          case '0xf871896b17e9cb7a64941c62c188a4f5c621b86800e3d15452ece01ce56073df':
+              logIndices = receipt.logs.reduce(
+                (_, log, index) =>
+                    ((log.topics[0].toLowerCase() === '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' &&
+                    log.topics[2].toLowerCase() === '0x0000000000000000000000000000000000000000000000000000000000000000') &&
+                    logIndices.push(index), logIndices), []
+            );
+            break;
 
           default:
             logIndices = receipt.logs.reduce(
