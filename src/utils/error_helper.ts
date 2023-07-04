fc22e3b4742628911b9a1f65f836f4bb142b16e1
Nitin Mittal <nitinmittal44066@gmail.com>
2023-07-05 02:27:53 +0400

    add: erc20 gasswapper contract for PoS

--- src/utils/error_helper.ts
+++ src/utils/error_helper.ts
@@ -30,6 +30,9 @@ export class ErrorHelper implements IError {
             case ERROR_TYPE.AllowedOnRoot:
                 errMsg = `The action ${info} is allowed only on root token.`;
                 break;
+            case ERROR_TYPE.AllowedOnMainnet:
+                errMsg = `The action is allowed only on mainnet chains.`;
+                break;
             case ERROR_TYPE.ProofAPINotSet:
                 errMsg = `Proof api is not set, please set it using "setProofApi"`;
                 break;
