f432e02f04496e00c795a0f1e63e5a39c410470e
dongjinlong <dongjinlong@outlook.com>
2024-03-26 22:41:33 +0800

    chore: fix error_type typo
    Signed-off-by: dongjinlong <dongjinlong@outlook.com>

--- src/enums/error_type.ts
+++ src/enums/error_type.ts
@@ -3,7 +3,7 @@ export enum ERROR_TYPE {
     AllowedOnChild = "allowed_on_child",
     Unknown = "unknown",
     ProofAPINotSet = "proof_api_not_set",
-    TransactionOptionNotObject = "transation_object_not_object",
+    TransactionOptionNotObject = "transaction_object_not_object",
     BurnTxNotCheckPointed = "burn_tx_not_checkpointed",
     EIP1559NotSupported = "eip-1559_not_supported",
     NullSpenderAddress = "null_spender_address",
