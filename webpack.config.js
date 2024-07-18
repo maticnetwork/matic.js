cd9c41012a08b3929fdededc82f4c04f81511609
Nitin Mittal <nitinmittal44066@gmail.com>
2024-07-18 19:33:05 +0400

    fix: audit fix and remove webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+/* global __dirname, require, module */
+const path = require('path')
+const webpack = require('webpack')
+const env = require('yargs').argv.env // use --env with webpack 2
+const banner = require('./license.js');
+
+const libraryName = 'Matic'
+
+let mode = 'development'
+
+if (env === 'build') {
+  mode = 'production'
+}
+
+const clientConfig = {
+  mode,
+  devtool: 'source-map',
+  entry: `${__dirname}/src/index.ts`,
+  target: 'web',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: `${libraryName}.umd.js`,
+    library: libraryName,
+    libraryTarget: 'umd',
+    // libraryExport: 'default',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+    web3: 'web3',
+    'ethereumjs-util': 'ethereumjs-util',
+    'query-string': 'query-string',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.json', '.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new webpack.BannerPlugin(banner)
+],
+}
+
+const serverConfig = {
+  ...clientConfig,
+  target: 'node',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: `${libraryName}.node.js`,
+    // globalObject: 'this',
+    libraryTarget: 'commonjs2',
+  },
+}
+
+const standaloneConfig = {
+  ...clientConfig,
+  output: {
+    ...clientConfig.output,
+    library: 'Matic',
+    filename: `${libraryName}.js`,
+  },
+  externals: {},
+}
+
+module.exports = [clientConfig, serverConfig, standaloneConfig]
